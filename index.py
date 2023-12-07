e0e6017e3bbb9fbc8085fc190ffeb9b0a32cf427
Arnold <Array0824@gmail.com>
2023-12-07 10:11:28 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,34 @@
+#first problem 
+#Write a program that takes in a user input and prints out how many vowels are in the response
+
+
+def vowels_now(vowels);
+    
+    vowels = aeiou
+    i = 0 
+    counter = 0 
+
+while i <= vowels.(leng)
+if vowels[i] in vowels
+i += 1 
+
+counter += 1
+
+return counter 
+
+vowels_now(example);
+vowels_now(today);
+
+
+#secon problem 
+#Using list comprehension, create a list of names of 4 letters or more and capitalize each name
+
+list_names [michael , joe , mark , josh]
+
+print(list_names.tittle)
+
+print("michael".(uppercase)
+print("joe".(uppercase)
+print("mark".(uppercase)
+print("josh".(uppercase)
+
